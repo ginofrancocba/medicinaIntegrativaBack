1233e4d32a9a815723ad29ff98fe31a6ec184124
ginofrancocba <ginofrancocba@gmail.com>
2023-10-31 23:47:11 -0300

    Add Change in Entity

--- src/main/java/com/clinica/medicinaIntegrativa/repository/ITurnoRepository.java
+++ src/main/java/com/clinica/medicinaIntegrativa/repository/ITurnoRepository.java
@@ -1,6 +1,5 @@
 package com.clinica.medicinaIntegrativa.repository;
 
-import com.clinica.medicinaIntegrativa.entity.Turno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
